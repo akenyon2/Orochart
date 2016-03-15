20695d248070dbb7e7e7f9ff71c3aa72ac01c9c5
Frozyn <eletrik090@gmail.com>
2016-03-14 20:16:58 -0400

    Edited all supplied files.
    Firstly, the bootstrap distribution was never committed, so make sure
    you have the 'dist' file located one level above the Orochart folder, or
    else none of the css will work.
    
    I've removed, altered, and organized all of the code. I removed the
    procedural sql in favor of PDOs.
    
    -Index.html was a pointless file.
    -Index.php serves as the homepage. Still working on adding more to it.
    -login.php requests the user to enter their email and password. If no
    such match is found in the database, then the user is notified that an
    email or password has been entered incorrectly.
    -register.php asks the user for various required information. Validation
    occurs on the php level. All fields must be filled, no email shall match
    any in the DB, PDOs with prepare statements ensure no injection can
    occur, etc. Further checks should be done and better user feedback is
    needed.
    -signUp.php was removed and replaced with register.php. Contains much or
    less the same code.
    -header-nav.php is the top nav bar.
    -login-validation.php and registration-validation.php contains the DB
    connections and queries for each respective file.
    -db/config.php contains the DB information

--- index.php
+++ index.php
@@ -1,20 +1,38 @@
- <!DOCTYPE html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <title>title</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Orochart - Homepage</title>
+
+    <!-- Bootstrap -->
+    <link href="../dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/default.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
   </head>
   <body>
-    <h1>First Page of Orochart</h1>
-    <?php
-        require_once('config.php'); 
-
-        echo '<a href="signUp.php"><p>Sign Up</p></a>';
-        echo '<a href="login.php"><p>Login</p></a>';
-
+    <?php 
+    include_once("includes/header-nav.php");
     ?>
+
+    <div class="container">
     <p>Users should now be able to create accounts. For working in the stack locally in phpMyAdmin, create a new table called user
        and create email varchar primary key, password varchar, firstName varchar, lastName varchar, and organization varchar<strong> is null</strong>.</p>
     <p>The login page needs some work, but that is for phase 3 anyway.</p>
+
+  </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../dist/js/bootstrap.min.js"></script>
   </body>
 </html>
--- login.php
+++ login.php
@@ -1,42 +1,39 @@
-<?php
-    require_once('config.php');
-    echo '<h1>hello and welcome to the login page</h1>';
-?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <!-- Bootstrap -->
+    <link href="../dist/css/bootstrap.min.css" rel="stylesheet">
 
-<form name="login" action="login.php" method="post" accept-charset="utf-8">
+</head>
+<body>
+    <?php
+        require_once('db/config.php');
+        include_once("includes/header-nav.php");
+        require_once("db/login-validation.php");
+    ?>
+    <div class="container">
+<form name="login" action="<?php echo $page ?>" method="POST" accept-charset="utf-8">
     <ul>
         <li>
-            <label for="mail">Email</label>
-            <input type="email" name="mail" placeholder="yourname@email.com" required>
+            <label for="Email">Email</label>
+            <input type="email" name="Email" placeholder="yourname@email.com" required>
         </li>
         <li>
-            <label for="pass">Password</label>
-            <input type="password" name="pass" placeholder="password" required>
+            <label for="Password">Password</label>
+            <input type="password" name="Password" placeholder="password" required>
         </li>
         <li>
         <input type="submit" value="Login">
         </li>
     </ul>
 </form>
-
-<?php
-    if (isset($_POST["mail"]) && ($_POST["pass"])){        
-        $email = $_POST["mail"];
-        $password = $_POST["pass"];
-
-        $sql = "SELECT *
-                FROM user
-                WHERE email=" . $email;
-
-        echo $sql;
-
-        if (($result = $conn->query($sql)) !== FALSE){
-            $row = $result->fetch_assoc();
-            
-            echo "<h1>Welcome back " . $row['firstName'] . "! You have successfully logged in.</h1></br>";
-
-        //header('LOCATION: index.php');
-        }
-        else {echo "something is wrong...";}
-    }
-?>
+</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../dist/js/bootstrap.min.js"></script>
+</body>
+</html>
